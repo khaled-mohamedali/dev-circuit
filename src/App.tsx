25056eb8f26fa50893eab9bec7145402dceac32e
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-04-03 16:55:35 -0400

    WebSite Layout built

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import { Box } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Box></Box>;
+  return (
+    <Grid
+      templateAreas={`"nav"
+                  "hero"
+                  "skills"
+                  "projects"
+                  "experience"
+                  "contact"
+                  "footer"`}
+      gridTemplateRows={"auto auto auto auto auto auto auto"}
+      gridTemplateColumns={"1fr"}
+      gap={6}
+      p={4}
+      bg="gray.500"
+    >
+      {/* Header */}
+      <GridItem area="nav" bg="white" boxShadow="md">
+        Nav
+      </GridItem>
+
+      {/* Hero Section */}
+      <GridItem area="hero">Hero</GridItem>
+
+      {/* Skills Section */}
+      <GridItem area="skills">Skills</GridItem>
+
+      {/* Projects Section */}
+      <GridItem area="projects">Projects</GridItem>
+
+      {/* Experience Section */}
+      <GridItem area="experience">Experience</GridItem>
+
+      {/* Contact Section */}
+      <GridItem area="contact">Contact</GridItem>
+
+      {/* Footer */}
+      <GridItem area="footer" bg="gray.800" color="white">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
