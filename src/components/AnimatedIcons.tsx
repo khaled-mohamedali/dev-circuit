9db81a377aad6bb7908ce22512c5513739339c23
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-04-05 14:27:31 -0400

    Inifinit Scrolling Built

--- src/components/AnimatedIcons.tsx
+++ src/components/AnimatedIcons.tsx
@@ -1,4 +1,5 @@
-import { HStack } from "@chakra-ui/react";
+import { animate, motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 import {
   FaCss3,
   FaCss3Alt,
@@ -8,8 +9,9 @@ import {
   FaReact,
 } from "react-icons/fa";
 import { FaC } from "react-icons/fa6";
-import SkillComponent from "./Skill";
 import { TbBrandCpp } from "react-icons/tb";
+import useMeasure from "react-use-measure";
+import SkillComponent from "./Skill";
 
 const skillIcons = [
   { id: 1, component: FaJava },
@@ -25,12 +27,27 @@ const skillIcons = [
 ];
 
 const AnimatedIcons = () => {
+  const [ref, { width }] = useMeasure();
+  const xTranslation = useMotionValue(0);
+
+  useEffect(() => {
+    let finalPosition = -width / 2;
+    let controls = animate(xTranslation, [0, finalPosition], {
+      ease: "linear",
+      duration: 10,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
+    return controls.stop;
+  }, [xTranslation, width]);
+
   return (
-    <HStack>
-      {[...skillIcons].map((skill, index) => (
+    <motion.div ref={ref} style={{ x: xTranslation }} className="gap-4">
+      {[...skillIcons, ...skillIcons].map((skill, index) => (
         <SkillComponent skill={skill}></SkillComponent>
       ))}
-    </HStack>
+    </motion.div>
   );
 };
 
