0c513a4d3990cd6b376f05f3073a8970e500f678
Khaled Mohamed Ali <mohamedk812.ka@gmail.com>
2025-04-08 18:46:11 -0400

    make logo responsive for the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,15 @@
-import { Box, HStack, Image } from "@chakra-ui/react";
-import Logo from "../assets/logo.svg";
+import { Box, HStack, useBreakpointValue } from "@chakra-ui/react";
+import { DevLogo } from "./Logo";
 import MobileMenu from "./MobileMenu";
 import NavLinks from "./NavLinks";
 
 const NavBar = () => {
+  const logoWidth = useBreakpointValue({ base: 150, md: 250 });
   return (
     <HStack justify="space-between">
-      <Box>
-        <Image src={Logo}></Image>
-      </Box>
+      {}
+
+      <DevLogo w={logoWidth ?? 100} h={50} />
       <NavLinks />
       <Box display={{ base: "flex", md: "none" }}>
         <MobileMenu />
